8c6cf7c2465ef28973b946df3e587fcbd757f74a
Oleg <rozomazan069@gmail.com>
2026-03-06 20:50:27 +0300

    change task2

--- solutions/sem02/lesson03/task2.py
+++ solutions/sem02/lesson03/task2.py
@@ -25,7 +25,8 @@ def convert_to_sphere(
 ) -> tuple[np.ndarray, np.ndarray, np.ndarray]:
     if abscissa.shape == ordinates.shape == applicates.shape:
         distances = np.sqrt(abscissa**2 + ordinates**2 + applicates**2)
-        azimuth = np.arctan2(ordinates, abscissa)
+        azimuth = np.zeros(distances.shape)
+        azimuth[distances > 0] = np.arctan2(ordinates[distances > 0], abscissa[distances > 0])
         inclination = np.zeros(distances.shape)
         inclination[distances > 0] = np.arccos(applicates[distances > 0] / distances[distances > 0])
 
