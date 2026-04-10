1f4cce3c624c97b7922f7be23d64168fea560b5b
Oleg <rozomazan069@gmail.com>
2026-04-10 17:47:02 +0300

    complete plt tasks

--- solutions/sem02/lesson07/task1.py
+++ solutions/sem02/lesson07/task1.py
@@ -13,8 +13,116 @@ def visualize_diagrams(
     ordinates: np.ndarray,
     diagram_type: Any,
 ) -> None:
-    # ваш код
-    pass
+    if abscissa.shape!=ordinates.shape:
+        raise ShapeMismatchError("shapes do not match")
+    if diagram_type not in ("hist","box","violin"):
+        raise ValueError("invalid diagram type")
+    
+    figure = plt.figure(figsize=(8, 8))
+    space=0.3
+    grid = plt.GridSpec(4, 4, wspace=space, hspace=space)
+
+    axis_scatter = figure.add_subplot(grid[:-1, 1:])
+    axis_scatter.scatter(abscissa, ordinates, color="cornflowerblue", alpha=0.5)
+
+    
+
+    if diagram_type=="hist":
+        axis_vert = figure.add_subplot(
+            grid[:-1, 0],
+            sharey=axis_scatter,
+        )
+        axis_hor = figure.add_subplot(
+            grid[-1, 1:],
+            sharex=axis_scatter,
+        )
+        axis_hor.hist(
+            abscissa,
+            bins=50,
+            color="red",
+            density=True,
+            alpha=0.5,
+        )
+        axis_vert.hist(
+            ordinates,
+            bins=50,
+            color="grey",
+            orientation="horizontal",
+            density=True,
+            alpha=0.5,
+        )
+        axis_hor.invert_yaxis()
+        axis_vert.invert_xaxis()
+        
+    if diagram_type=="violin":
+        axis_vert=figure.add_subplot(
+            grid[:-1, 0],
+            sharey=axis_scatter
+        )
+        violin_parts_vert=axis_vert.violinplot(
+            ordinates,
+            vert=True,
+            showmedians=True
+        )
+        for body in violin_parts_vert["bodies"]:
+            body.set_facecolor("grey")
+            body.set_edgecolor("darkgrey")
+
+        for part in violin_parts_vert:
+            if part == "bodies":
+                continue
+
+            violin_parts_vert[part].set_edgecolor("grey")
+
+        axis_hor=figure.add_subplot(
+            grid[-1, 1:],
+            sharex=axis_scatter,
+        )
+        violin_parts_hor=axis_hor.violinplot(
+            abscissa,
+            vert=False,
+            showmedians=True
+        )
+        for body in violin_parts_hor["bodies"]:
+            body.set_facecolor("red")
+            body.set_edgecolor("orange")
+
+        for part in violin_parts_hor:
+            if part == "bodies":
+                continue
+
+            violin_parts_hor[part].set_edgecolor("red")        
+    
+    if diagram_type=="box":
+        axis_vert=figure.add_subplot(
+            grid[:-1, 0],
+            sharey=axis_scatter,
+        )
+        axis_vert.boxplot(
+            ordinates,
+            vert=True,
+            patch_artist=True,
+            boxprops=dict(facecolor="grey"),
+            medianprops=dict(color="black"),
+        )
+        axis_hor=figure.add_subplot(
+            grid[-1, 1:],
+            sharex=axis_scatter,
+        )
+        axis_hor.boxplot(
+            abscissa,
+            vert=False,
+            patch_artist=True,
+            boxprops=dict(facecolor="red"),
+            medianprops=dict(color="yellow"),
+        )
+
+
+
+
+    
+    
+
 
 
 if __name__ == "__main__":
@@ -24,5 +132,5 @@ if __name__ == "__main__":
 
     abscissa, ordinates = np.random.multivariate_normal(mean, cov, size=1000).T
 
-    visualize_diagrams(abscissa, ordinates, "hist")
+    visualize_diagrams(abscissa, ordinates, "violin")
     plt.show()
--- solutions/sem02/lesson07/task2.py
+++ solutions/sem02/lesson07/task2.py
@@ -1,1 +1,56 @@
-# ваш код (используйте функции или классы для решения данной задачи)
+import json
+
+import matplotlib.pyplot as plt
+import numpy as np
+
+
+def visualise_progress(path):
+    with open(path, 'r', encoding='utf-8') as f:
+        data=json.load(f)
+
+    if "before" not in data or "after" not in data:
+        raise ValueError("wrong data format")
+        
+    
+
+    all_labels = ['I', 'II', 'III', 'IV']
+    counts_before = [data["before"].count(symb) for symb in all_labels]  
+    counts_after = [data["after"].count(symb) for symb in all_labels]    
+    stages=np.arange(4)
+    width=0.4
+
+
+    plt.style.use('seaborn-v0_8-darkgrid')
+    figure, axis = plt.subplots(figsize=(12, 7))
+    
+    axis.set_title("Mitral diseases stages", fontsize=17, fontweight="bold", c="dimgray")
+    axis.set_ylabel("amount of people", fontsize=15, fontweight="bold", c="dimgray")
+    axis.set_xlabel("stages of diseases", fontsize=15, fontweight="bold", c="dimgray")
+        
+    axis.tick_params(axis="x", labelsize=14, labelcolor="dimgray")
+
+    axis.bar(
+        stages-width/2,
+        counts_before,
+        width=width,
+        color="green",
+        edgecolor="limegreen",
+        label="before"
+    )
+    axis.bar(
+        stages+width/2,
+        counts_after,
+        width=width,
+        color="tomato",
+        edgecolor="red",
+        label="after"
+    )
+    axis.set_xticks(
+        stages,
+        labels=all_labels,
+        weight="bold",
+    )
+    axis.legend(fontsize=15)
+       
+    figure.savefig("cardio_progress.png", bbox_inches="tight")
+    plt.show()     
